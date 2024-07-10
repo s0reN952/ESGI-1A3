e06d76fa62d71a150a2c8e54b209f7fd89f95cd5
ilyas <ilyas.salek@gmail.com>
2024-07-10 21:38:45 +0200

    mise a jour du .env de notification

--- notification-service/test_mail.php
+++ notification-service/test_mail.php
@@ -5,16 +5,16 @@ use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Mailer\Transport;
 use Symfony\Component\Mime\Email;
 
-// Définir la variable d'environnement MAILER_DSN directement dans le script
+/
 putenv('MAILER_DSN=smtp://100.moonnaie@gmail.com:q%20y%20r%20g%20v%20e%20s%20e%20p%20t%20g%20j%20z%20h%20u%20m@smtp.gmail.com:587?encryption=tls');
 
-$dsn = getenv('MAILER_DSN');  // Récupère la valeur de MAILER_DSN depuis l'environnement
+$dsn = getenv('MAILER_DSN');  
 $transport = Transport::fromDsn($dsn);
 $mailer = new Mailer($transport);
 
 $email = (new Email())
-    ->from('100.moonnaie@gmail.com')  // Remplacez par votre adresse email d'expéditeur
-    ->to('ilyas.salek@gmail.com') // Remplacez par l'adresse email du destinataire
+    ->from('100.moonnaie@gmail.com')  
+    ->to('ilyas.salek@gmail.com') 
     ->subject('Test from Symfony Mailer!')
     ->text('This is a test email sent from a standalone PHP script.');
 
